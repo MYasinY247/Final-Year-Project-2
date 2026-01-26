b882cbe48d1e5449a86ceb31ad8faffaaba1f588
Yasin <w1974891@westminster.ac.uk>
2026-01-26 18:38:27 +0000

    Initial commit

---
+++ FoodSuitabilityScanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodSuitabilityScanner
+//
+//  Created by Muhammad Yasin Yahya on 26/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoodSuitabilityScanner/FoodSuitabilityScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodSuitabilityScannerApp.swift
+//  FoodSuitabilityScanner
+//
+//  Created by Muhammad Yasin Yahya on 26/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct FoodSuitabilityScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
